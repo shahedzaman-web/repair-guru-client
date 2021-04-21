677643aa651e789ff9f84aba72c2d790792a03f7
shahedzaman-web <physics.shahedzaman@gmail.com>
2021-04-21 16:02:15 +0600

    3rd commit

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -1,66 +1,34 @@
-import React from 'react';
-import './Footer.css';
-import FooterCol from '../FooterCol/FooterCol';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faInstagram, faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
-
+import React from "react";
+import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Footer = () => {
-    const noNamed = [
-        {name: "Emergency Dental Care" , link: "/emergency"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Treatment of Personal Diseases" , link: "/personal-treatment"},
-        {name: "Tooth Extraction" , link: "/tooth-extract"},
-        {name: "Check Up" , link: "/checkup"},
-    ]
-    const ourAddress = [
-        {name: "New York - 101010 Hudson" , link: "//google.com/map"},
-        {name: "Yards" , link: "//google.com/map"},
-       
-    ]
-    const oralHealth = [
-        {name: "Emergency Dental Care" , link: "/emergency"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Treatment of Personal Diseases" , link: "/personal-treatment"},
-        {name: "Tooth Extraction" , link: "/tooth-extract"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Check Up" , link: "/checkup"}
-    ]
-    const services = [
-        {name: "Emergency Dental Care" , link: "/emergency"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Treatment of Personal Diseases" , link: "/personal-treatment"},
-        {name: "Tooth Extraction" , link: "/tooth-extract"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Check Up" , link: "/checkup"},
-        {name: "Check Up" , link: "/checkup"}
-    ]
-    return (
-        <footer className="footer-area clear-both">
-            <div className="container pt-5">
-                <div className="row py-5">
-                    <FooterCol key={1} menuTitle={"."} menuItems={noNamed}/>
-                    <FooterCol key={2} menuTitle="Services" menuItems={services}/>
-                    <FooterCol key={3} menuTitle="Oral Health" menuItems={oralHealth}/>
-                    <FooterCol key={4} menuTitle="Our Address" menuItems={ourAddress}> 
-                        <ul className="social-media list-inline">
-                            <li className="list-inline-item"><a href="//facebook.com"><FontAwesomeIcon className="icon active-icon" icon={faFacebookF} /></a></li>
-                            <li className="list-inline-item"><a href="//google.com"><FontAwesomeIcon className="icon" icon={faGooglePlusG} /></a></li>
-                            <li className="list-inline-item"><a href="//instagram.com"><FontAwesomeIcon className="icon" icon={faInstagram} /></a></li>
-                        </ul>
-                        <div className="mt-5">
-                            <h6>Call now</h6>
-                            <button className="btn btn-primary">+2025550295</button>
-                        </div>
-                    </FooterCol>
-                </div>
-                <div className="copyRight text-center">
-                    <p>Copyright {(new Date()).getFullYear()} All Rights Reserved</p>
-                </div>
-            </div>
-        </footer>
-        
-    );
+  return (
+    <footer className=" footer-style">
+      <div className="row container">
+        <div className="col-8">
+          <p className="p-2">&#169; All Right Reserved. Md. Shaheduzzaman</p>
+        </div>
+        <div className="col-2"></div>
+        <div className="col-2 ">
+          <a
+            className="rounded"
+            target="_blank"
+            href={"https://www.facebook.com/Shahedxxaman/"}
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "facebook-f"]} />
+          </a>
+          <a
+            target="_blank"
+            href={"https://github.com/shahedzaman-web"}
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={["fab", "github"]} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
